0155c114dd552100b937b27a3d0c34da48c5fd26
Senthilkumaranshan <skdudeflow@gmail.com>
2021-05-12 08:48:52 +0530

    Reverted changes in DbConnection

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -1,8 +1,7 @@
 import java.sql.SQLException;
 
 import java.sql.*;
-//test
-//test2
+
 public class DbConnection {
 	
     private final static String url = "jdbc:mysql://localhost:3306/foodlog?autoReconnect=true&useSSL=false";
